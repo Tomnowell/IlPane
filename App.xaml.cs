61e926346c6062baad39b852810839e7336b8eb1
Tom Nowell <thomnowell@gmail.com>
2021-04-26 22:43:54 +0900

    Add some DB connection code

--- App.xaml.cs
+++ App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using DataAccessLibrary;
 
 namespace Pane
 {
@@ -30,6 +31,8 @@ namespace Pane
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+
+            DataAccess.InitializeDatabase();
         }
 
         /// <summary>
